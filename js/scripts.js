10bf247355559e957a2190f183191ea34f79720a
Reon Wilson <106224850+reonwilson0401@users.noreply.github.com>
2025-05-11 22:46:19 -0500

    Add files via upload

---
+++ js/scripts.js
@@ -0,0 +1,25 @@
+function hamburger() {
+    var menu = document.getElementById("menu-links");
+    if (menu.style.display === "block") {
+        menu.style.display = "none";
+    } else {
+        menu.style.display = "block";
+    }
+}
+
+let mybutton = document.getElementById("myBtn");
+
+window.onscroll = function(){scrollFunction()};
+
+function scrollFunction(){
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        mybutton.style.display = "block";
+    } else {
+        mybutton.style.display = "none";
+    }
+}
+
+function topFunction() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
